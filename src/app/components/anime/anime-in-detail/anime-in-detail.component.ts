93c31c5088321e7e13d4dd9d4fd4c55c5b41a604
prabhas utla <prabashutla@111gmail.com>
2024-07-18 07:25:35 +0530

    id changes issue is fix

--- src/app/components/anime/anime-in-detail/anime-in-detail.component.ts
+++ src/app/components/anime/anime-in-detail/anime-in-detail.component.ts
@@ -25,33 +25,34 @@ export class AnimeInDetailComponent implements OnInit {
   getdetailanime() {
     this.mal_id = this.activatedrouter.snapshot.paramMap.get('mal_id')
     console.log("id", this.mal_id)
-    this.mal_id && this.api.detailanime(this.mal_id).subscribe((res: any) => { this.detailanime = res, console.log(this.detailanime) }, (err) => { console.log(err) });
-    this.mal_id && this.api.animerecommdation(this.mal_id).subscribe((res: any) => { this.recommadation = res, console.log(res) }, (err) => { console.log(err) })
-    this.mal_id && this.api.animevideos(this.mal_id).subscribe((res) => { this.trailer = res, console.log(res) }, (err) => { console.log(err) })
+    this.api.detailanime(this.mal_id).subscribe((res: any) => { this.detailanime = res, console.log(this.detailanime) }, (err) => { console.log(err) });
+    this.api.animerecommdation(this.mal_id).subscribe((res: any) => { this.recommadation = res, console.log(res) }, (err) => { console.log(err) })
+    this.api.animevideos(this.mal_id).subscribe((res) => { this.trailer = res, console.log(res) }, (err) => { console.log(err) })
   }
 
-  getrecommdationanime() {
-    this.router.navigateByUrl("/anime/this.recommadation.data.entry.mal_id")
-    location.reload()
-    this.getdetailanime()
-    let mal_id = this.activatedrouter.snapshot.paramMap.get('mal_id')
-    console.log("id", mal_id)
-    // mal_id && this.api.detailanime(mal_id).subscribe((res:any)=>{this.detailanime=res,console.log(this.detailanime)},(err)=>{console.log(err)});
-    // mal_id && this.api.animerecommdation(mal_id).subscribe((res)=>{this.recommadation=res,console.log(res)},(err)=>{console.log(err)})
-
-  }
+  // getrecommdationanime() {
+  //   this.router.navigateByUrl("/anime/this.recommadation.data.entry.mal_id")
+  //   location.reload()
+  //   this.getdetailanime()
+  //   let mal_id = this.activatedrouter.snapshot.paramMap.get('mal_id')
+  //   console.log("id", mal_id)
+  //   // mal_id && this.api.detailanime(mal_id).subscribe((res:any)=>{this.detailanime=res,console.log(this.detailanime)},(err)=>{console.log(err)});
+  //   // mal_id && this.api.animerecommdation(mal_id).subscribe((res)=>{this.recommadation=res,console.log(res)},(err)=>{console.log(err)})
+  //   this.router.navigateByUrl(this.router.url.replace(this.mal_id, mal_id));
+  // }
 
 
   getRecomdation(id: any) {
     let idx = id.entry.mal_id
+    console.log("id", this.mal_id)
+    this.router.navigateByUrl(`/anime/${idx}`); //this is direct way to changes route
+    // this.router.navigateByUrl(this.router.url.replace(this.mal_id, idx)) //this is replace route id
     console.log(idx)
     this.mal_id = idx
-    console.log("id", this.mal_id)
     this.mal_id && this.api.detailanime(this.mal_id).subscribe((res: any) => { this.detailanime = res, console.log(this.detailanime) }, (err) => { console.log(err) });
     this.mal_id && this.api.animerecommdation(this.mal_id).subscribe((res) => { this.recommadation = res, console.log(res) }, (err) => { console.log(err) })
     this.mal_id && this.api.animevideos(this.mal_id).subscribe((res) => { this.trailer = res, console.log(res) }, (err) => { console.log(err) })
-
-    this.router.navigateByUrl(this.router.url.replace(this.mal_id, idx))
+    
   }
 
   addwatchlish(item: any) {
