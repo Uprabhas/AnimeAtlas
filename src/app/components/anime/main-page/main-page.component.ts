646d06d83772266f3058d95d3111ec807c2cc1cb
prabhas utla <prabashutla@111gmail.com>
2024-08-10 08:49:56 +0530

    all Ui changes for mobile view

--- src/app/components/anime/main-page/main-page.component.ts
+++ src/app/components/anime/main-page/main-page.component.ts
@@ -14,34 +14,26 @@ import { data } from '../../models/model';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-  Topanime: data[] | any = [];
+  Topanime: any[] = [];
   title: any;
   searchanimeresult: any;
   // addlist: boolean = true;
   // removelist: boolean = false
   showtop: boolean = true;
-  showsearch: boolean = false
+  showsearch: boolean = false;
   pageadd: any = 1;
-  wishlist:any;
-
-
-
+  wishlist: any;
 
   private page$ = new BehaviorSubject(1);
 
-  constructor(private api: ApiService, private router: Router) {
-
-  }
+  constructor(private api: ApiService, private router: Router) {}
   ngOnInit(): void {
     if (this.title! == null) {
       this.getTopanime(this.pageadd);
     } else {
-      this.searchanime(this.title, this.pageadd)
-
+      this.searchanime(this.title, this.pageadd);
     }
   }
-
-  
 
   // public loadProducts$(): Observable<ProductsPaginator> {
   //   return this.page$.pipe(
@@ -70,77 +62,80 @@ export class MainPageComponent implements OnInit {
   //   this.page$.next(paginator.page + 1);
   // }
 
-
-
-
   onsearch(search: any) {
-    this.title = search
-    console.log(search)
-    this.searchanime(search, this.pageadd)
+    this.title = search;
+    console.log(search);
+    this.searchanime(search, this.pageadd);
     this.showtop = false;
     this.showsearch = true;
   }
 
-
-
   getTopanime(pageno: any) {
-    this.api.topAnime(pageno).subscribe((res: data[]) => { this.Topanime = res, console.log(this.Topanime) }, (err) => { console.log(err) })
+    this.api.topAnime(pageno).subscribe(
+      (res: any) => {
+        (this.Topanime = res?.data), console.log(this.Topanime);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
   searchanime(name: any, page: any) {
     setTimeout(() => {
-      this.api.searchable(name, page).subscribe((res: any) => { this.searchanimeresult = res.data, console.log(res) }, (err) => { console.log(err) })
+      this.api.searchable(name, page).subscribe(
+        (res: any) => {
+          (this.searchanimeresult = res.data), console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
     }, 1000);
   }
 
-  addwatchlish(item: any,id:any,i:any) {
-    console.log(i)
-    this.api.Addwatchlistanime(item)
-    this.api.listanime().subscribe((res:any)=>{console.log(res),this.wishlist=res});
-    console.log(id)
+  addwatchlish(item: any, id: any, i: any) {
+    console.log(i);
+    this.api.Addwatchlistanime(item);
+    this.api.listanime().subscribe((res: any) => {
+      console.log(res), (this.wishlist = res);
+    });
+    console.log(id);
     this.wishlist.forEach((data: any) => {
-      if(data.mal_id===id){
+      if (data.mal_id === id) {
         // this.addlist=false;
         // this.removelist=true
       }
     });
   }
 
-  removewatchlist(item: any,id:any) {
+  removewatchlist(item: any, id: any) {
     this.api.removewatchlistanime(item);
     // this.addlist = true;
     // this.removelist = false;
-    console.log(id)
+    console.log(id);
   }
 
-
   addpage() {
-    this.pageadd += 1
-    console.log(this.pageadd)
+    this.pageadd += 1;
+    console.log(this.pageadd);
     this.getTopanime(this.pageadd);
-    this.searchanime(this.title, this.pageadd)
+    this.searchanime(this.title, this.pageadd);
   }
 
   subpage() {
-    this.pageadd -= 1
-    console.log(this.pageadd)
+    this.pageadd -= 1;
+    console.log(this.pageadd);
     this.getTopanime(this.pageadd);
-    this.searchanime(this.title, this.pageadd)
+    this.searchanime(this.title, this.pageadd);
   }
 
   onScroll() {
-    this.api.topAnime(++this.pageadd)
-      .subscribe((data: data[]) => {
-        this.Topanime.push(...data);
-        console.log(this.Topanime)
-      });
+    // this.api.topAnime(++this.pageadd).subscribe((data: data[]) => {
+    //   this.Topanime.push(data);
+    //   this.pageadd += 1;
+    //   console.log(this.pageadd);
+    //   console.log(this.Topanime);
+    // });
   }
-
-
-
-
-
-
-
 }
-
